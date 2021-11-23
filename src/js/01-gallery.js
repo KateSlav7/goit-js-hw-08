043b68db5f6478151f3ab0329583dedcc9dad7a9
KateSlav7 <kateslavgorodskaya@gmail.com>
2021-11-23 15:33:39 +0200

    /

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,41 +1,27 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
-const galleryDivEl = document.querySelector(".gallery");
-const cardsMarkup = createCardsMarkup(galleryItems);
-console.log(galleryDivEl);
-
-galleryDivEl.insertAdjacentHTML("beforeend", cardsMarkup);
-
-function createCardsMarkup(item) {
-  return item
-    .map(({ preview, original, description }) => {
-      return `
-    <div class="gallery__item">
-    <li>
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-        </a>
-        </li>
-    </div>    
-    `;
-    })
-    .join("");
-}
-
-  let gallery = new SimpleLightbox(".gallery a", {
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgCardsMarkup(galleryItems);
+const optionsLightBox = {
     captions: true,
     captionsData: "alt",
     captionDelay: 250,
-  });
+};
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createImgCardsMarkup(galleryItems) {
+   return  galleryItems.map(({ preview, original, description }) => {
+       return `
+            <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+    }).join("");
+};
+
+const lightbox = new SimpleLightbox(".gallery a", optionsLightBox);
