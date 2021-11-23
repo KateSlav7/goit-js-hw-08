3a9d03ba04ce406e70e1465cc880bbda9f9c2ce5
KateSlav7 <kateslavgorodskaya@gmail.com>
2021-11-23 14:15:09 +0200

    .

---
+++ src/js/01-gallery.js
@@ -0,0 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryDivEl = document.querySelector(".gallery");
+const cardsMarkup = createCardsMarkup(galleryItems);
+console.log(galleryDivEl);
+
+galleryDivEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createCardsMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <li>
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li>
+    </div>    
+    `;
+    })
+    .join("");
+}
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
