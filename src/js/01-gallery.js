676a81551b4a612658d3bc295400df1a15758a2d
KateSlav7 <kateslavgorodskaya@gmail.com>
2021-11-25 11:11:56 +0200

    correction

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,33 @@
 
-import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(galleryItems);
+
+import { galleryItems } from './gallery-items';
+
 const galleryDivEl = document.querySelector('.gallery');
-const cardsMarkup = createCardsMarkup(galleryItems);
-console.log(galleryDivEl);
-galleryDivEl.insertAdjacentHTML('beforeend', cardsMarkup);
-function createCardsMarkup(item) {
-return item
-.map(({ preview, original, description }) => {
-return `
-<div class="gallery__item"><li>
-<a class="gallery__link" href="${original}">
-<img
-class="gallery__image"
-src="${preview}"
-data-source="${original}"
-alt="${description}"/></a></li></div>    
-`;
-})
-.join('');
-}
-galleryDivEl.addEventListener('click', onSelectImage);
-function onSelectImage(evt) {
-evt.preventDefault();
-if (evt.target.nodeName !== 'IMG') {
-return;
-}
-const imgUrl = evt.target.dataset.source;
-let gallery = new SimpleLightbox('.gallery a', {
-captions: true,
-captionsData: 'alt',
-captionDelay: 250,
+const cardsMarkup = galleryItems.map(
+    ({ preview, original, description }) =>
+        `<li class="gallery__link">
+        <a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}" />
+        </a>
+        </li>`
+).join('');
+
+galleryDivEl.insertAdjacentHTML('beforeend', cardsMarkup)
+
+let lightbox = new SimpleLightbox(".gallery__item", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  disableScroll: true,
 });
-}
+
+lightbox.on('closed.simplelightbox', function () {
+ console.log('Спасибо за уделенное время');   
+});
+console.log(galleryItems);
